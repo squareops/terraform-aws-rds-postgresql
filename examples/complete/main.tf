79a953ba0f6b0df231f7dcc15656725bb5c1029d
divyanshujainSquareops <148210383+divyanshujainSquareops@users.noreply.github.com>
2024-11-18 16:16:04 +0530

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -139,11 +139,11 @@ module "rds-pg" {
   postgresdb_backup_config = {
     postgres_database_name = ""                             # Specify the database name or Leave empty if you wish to backup all databases
     cron_for_full_backup   = "*/2 * * * *"                  # set cronjob for backup
-    bucket_uri             = "s3://mongodb-backups-atmosly" # s3 bucket uri
+    bucket_uri             = "s3://postgres-backups-atmosly" # s3 bucket uri
   }
   postgresdb_restore_enabled = false
   postgresdb_restore_config = {
-    bucket_uri       = "s3://mongodb-backups-atmosly" #S3 bucket URI (without a trailing slash /) containing the backup dump file.
+    bucket_uri       = "s3://postgres-backups-atmosly" #S3 bucket URI (without a trailing slash /) containing the backup dump file.
     backup_file_name = "db5_20241114111607.sql"       #Give .sql or .zip file for restore
   }
 }
