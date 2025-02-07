aa63d18628f2c8c372c664e20cc159637dc368b0
amanravi-squareops <aman.ravi@squareops.comm>
2025-02-07 17:02:49 +0530

    fixed restore database

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -6,7 +6,7 @@ locals {
   environment             = "prod"
   create_namespace        = true
   namespace               = "pg"
-  engine_version          = "15.4"
+  engine_version          = "15.7"
   instance_class          = "db.t4g.micro"
   storage_type            = "gp3"
   cluster_name            = ""
@@ -92,7 +92,7 @@ module "vpc" {
   name                    = local.name
   vpc_cidr                = local.vpc_cidr
   environment             = local.environment
-  availability_zones      = ["us-east-1a", "us-east-1b"]
+  availability_zones      = ["us-east-2a", "us-east-2b"]
   public_subnet_enabled   = true
   auto_assign_public_ip   = true
   intra_subnet_enabled    = false
@@ -104,14 +104,14 @@ module "vpc" {
 module "rds-pg" {
   source                           = "squareops/rds-postgresql/aws"
   version                          = "2.0.0"
-  name                             = local.name
-  db_name                          = "test"
-  multi_az                         = false
-  family                           = local.family
+  name     = local.name
+  db_name  = "test"
+  multi_az = false
+  family   = local.family
   vpc_id                           = module.vpc.vpc_id
-  allowed_security_groups          = local.allowed_security_groups
+  allowed_security_groups = local.allowed_security_groups
   subnet_ids                       = module.vpc.database_subnets
-  environment                      = local.environment
+  environment = local.environment
   kms_key_arn                      = module.kms.key_arn
   storage_type                     = local.storage_type
   engine_version                   = local.engine_version
@@ -141,13 +141,14 @@ module "rds-pg" {
   create_namespace          = local.create_namespace
   postgresdb_backup_enabled = false
   postgresdb_backup_config = {
-    postgres_database_name = ""                              # Specify the database name or Leave empty if you wish to backup all databases
-    cron_for_full_backup   = "*/2 * * * *"                   # set cronjob for backup
-    bucket_uri             = "s3://postgres-backups-atmosly" # s3 bucket uri
+    postgres_database_name = ""                    # Specify the database name or Leave empty if you wish to backup all databases
+    cron_for_full_backup   = "*/2 * * * *"                    # set cronjob for backup
+    bucket_uri             = "s3://my-backup-dumps-databases" # s3 bucket uri
   }
-  postgresdb_restore_enabled = false
+  postgresdb_restore_enabled = true
   postgresdb_restore_config = {
-    bucket_uri       = "s3://postgres-backups-atmosly" #S3 bucket URI (without a trailing slash /) containing the backup dump file.
-    backup_file_name = "db5_20241114111607.sql"        #Give .sql or .zip file for restore
+    bucket_uri       = "s3://my-backup-dumps-databases" #S3 bucket URI (without a trailing slash /) containing the backup dump file.
+    backup_file_name = "atmosly_db1.sql"                #Give .sql or .zip file for restore
   }
 }
+
