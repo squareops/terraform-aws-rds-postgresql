61d27f6be46c83b0bab021eed6f9077f60b6a293
Aman <146931382+amanravi-squareops@users.noreply.github.com>
2025-03-19 12:38:29 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -347,10 +347,9 @@ variable "postgresdb_backup_config" {
   type = map(string)
   default = {
     bucket_uri = ""
-    # s3_bucket_region     = ""
     cron_for_full_backup   = ""
     postgres_database_name = ""
-    # db_endpoint=""
+
   }
   description = "configuration options for MySQL database backups. It includes properties such as the S3 bucket URI, the S3 bucket region, and the cron expression for full backups."
 }
@@ -360,7 +359,6 @@ variable "postgresdb_restore_config" {
   default = {
     bucket_uri = ""
     file_name  = ""
-    # s3_bucket_region = ""
   }
   description = "Configuration options for restoring dump to the MySQL database."
 }
